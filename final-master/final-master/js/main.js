8408b30cc2ef762acbe2f3898e384b97f1d92d86
madisonstevens <ms2652@cornell.edu>
2016-11-29 12:43:51 -0500

    Add files via upload
    Centered the Calendar button on the home page, added some text, and updated the color scheme

---
+++ final-master/final-master/js/main.js
@@ -0,0 +1,12 @@
+function initMap() {
+	//https://developers.google.com/maps/documentation/javascript/adding-a-google-map#key
+	var commons = {lat:42.4395928, lng:-76.4991818};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 16,
+		center: commons
+	});
+    var marker = new google.maps.Marker({
+        position: commons,
+        map: map
+    });
+}
